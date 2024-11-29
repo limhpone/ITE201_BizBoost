1f658f148b24b06f2f5a228e4aea6ceaef20fd05
limhpone <158800872+limhpone@users.noreply.github.com>
2024-11-30 05:15:47 +0700

    Update 6_📷️_Live_Stream.py

--- pages/6_📷️_Live_Stream.py
+++ pages/6_📷️_Live_Stream.py
@@ -737,127 +737,120 @@ class ProcessFrame:
 
                         
 
+def get_thresholds_beginner():
 
-        def get_thresholds_beginner():
-    
-            _ANGLE_HIP_KNEE_VERT = {
-                                    'NORMAL' : (0,  32),
-                                    'TRANS'  : (35, 65),
-                                    'PASS'   : (70, 95)
-                                }    
-    
-                
-            thresholds = {
-                            'HIP_KNEE_VERT': _ANGLE_HIP_KNEE_VERT,
-    
-                            'HIP_THRESH'   : [10, 50],
-                            'ANKLE_THRESH' : 45,
-                            'KNEE_THRESH'  : [50, 70, 95],
-    
-                            'OFFSET_THRESH'    : 35.0,
-                            'INACTIVE_THRESH'  : 15.0,
-    
-                            'CNT_FRAME_THRESH' : 50
-                                    
-                        }
-    
-            return thresholds
-    
-    
-    
-        # Get thresholds for beginner mode
-        def get_thresholds_pro():
-    
-            _ANGLE_HIP_KNEE_VERT = {
-                                    'NORMAL' : (0,  32),
-                                    'TRANS'  : (35, 65),
-                                    'PASS'   : (80, 95)
-                                }    
-    
-                
-            thresholds = {
-                            'HIP_KNEE_VERT': _ANGLE_HIP_KNEE_VERT,
-    
-                            'HIP_THRESH'   : [15, 50],
-                            'ANKLE_THRESH' : 30,
-                            'KNEE_THRESH'  : [50, 80, 95],
-    
-                            'OFFSET_THRESH'    : 35.0,
-                            'INACTIVE_THRESH'  : 15.0,
-    
-                            'CNT_FRAME_THRESH' : 50
-                                    
-                        }
-                        
-            return thresholds
-    
-        st.title('AI Fitness Trainer: Squats Analysis')
-        
-        mode = st.radio('Select Mode', ['Beginner', 'Pro'], horizontal=True)
-        
-        thresholds = None 
-        
-        if mode == 'Beginner':
-            thresholds = get_thresholds_beginner()
-        
-        elif mode == 'Pro':
-            thresholds = get_thresholds_pro()
-        
-        
-        live_process_frame = ProcessFrame(thresholds=thresholds, flip_frame=True)
-        # Initialize face mesh solution
-        pose = get_mediapipe_pose()
-        
-        
-        if 'download' not in st.session_state:
-            st.session_state['download'] = False
-        
-        output_video_file = f'output_live.flv'
-        
-          
-        
-        def video_frame_callback(frame: av.VideoFrame):
-            frame = frame.to_ndarray(format="rgb24")  # Decode and get RGB frame
-            frame, _ = live_process_frame.process(frame, pose)  # Process frame
-            return av.VideoFrame.from_ndarray(frame, format="rgb24")  # Encode and return BGR frame
-        
-        
-        def out_recorder_factory() -> MediaRecorder:
-                return MediaRecorder(output_video_file)
-        
-        
-        ctx = webrtc_streamer(
-                                key="Squats-pose-analysis",
-                                video_frame_callback=video_frame_callback,
-                                rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},  # Add this config
-                                media_stream_constraints={"video": {"width": {'min':480, 'ideal':480}}, "audio": False},
-                                video_html_attrs=VideoHTMLAttributes(autoPlay=True, controls=False, muted=False),
-                                out_recorder_factory=out_recorder_factory
-                            )
-        
-        
-        download_button = st.empty()
-        
-        if os.path.exists(output_video_file):
-            with open(output_video_file, 'rb') as op_vid:
-                download = download_button.download_button('Download Video', data = op_vid, file_name='output_live.flv')
-        
-                if download:
-                    st.session_state['download'] = True
-        
-        
-        
-        if os.path.exists(output_video_file) and st.session_state['download']:
-            os.remove(output_video_file)
-            st.session_state['download'] = False
-            download_button.empty()
-        
-        
-        
-    
-    
-    
-    
+        _ANGLE_HIP_KNEE_VERT = {
+                                'NORMAL' : (0,  32),
+                                'TRANS'  : (35, 65),
+                                'PASS'   : (70, 95)
+                            }    
+
+            
+        thresholds = {
+                        'HIP_KNEE_VERT': _ANGLE_HIP_KNEE_VERT,
+
+                        'HIP_THRESH'   : [10, 50],
+                        'ANKLE_THRESH' : 45,
+                        'KNEE_THRESH'  : [50, 70, 95],
+
+                        'OFFSET_THRESH'    : 35.0,
+                        'INACTIVE_THRESH'  : 15.0,
+
+                        'CNT_FRAME_THRESH' : 50
+                                
+                    }
+
+        return thresholds
+
+
+
+    # Get thresholds for beginner mode
+def get_thresholds_pro():
+
+        _ANGLE_HIP_KNEE_VERT = {
+                                'NORMAL' : (0,  32),
+                                'TRANS'  : (35, 65),
+                                'PASS'   : (80, 95)
+                            }    
+
+            
+        thresholds = {
+                        'HIP_KNEE_VERT': _ANGLE_HIP_KNEE_VERT,
+
+                        'HIP_THRESH'   : [15, 50],
+                        'ANKLE_THRESH' : 30,
+                        'KNEE_THRESH'  : [50, 80, 95],
+
+                        'OFFSET_THRESH'    : 35.0,
+                        'INACTIVE_THRESH'  : 15.0,
+
+                        'CNT_FRAME_THRESH' : 50
+                                
+                    }
+                    
+        return thresholds
+
+st.title('AI Fitness Trainer: Squats Analysis')
+
+mode = st.radio('Select Mode', ['Beginner', 'Pro'], horizontal=True)
+
+thresholds = None 
+
+if mode == 'Beginner':
+    thresholds = get_thresholds_beginner()
+
+elif mode == 'Pro':
+    thresholds = get_thresholds_pro()
+
+
+live_process_frame = ProcessFrame(thresholds=thresholds, flip_frame=True)
+# Initialize face mesh solution
+pose = get_mediapipe_pose()
+
+
+if 'download' not in st.session_state:
+    st.session_state['download'] = False
+
+output_video_file = f'output_live.flv'
+
+  
+
+def video_frame_callback(frame: av.VideoFrame):
+    frame = frame.to_ndarray(format="rgb24")  # Decode and get RGB frame
+    frame, _ = live_process_frame.process(frame, pose)  # Process frame
+    return av.VideoFrame.from_ndarray(frame, format="rgb24")  # Encode and return BGR frame
+
+
+def out_recorder_factory() -> MediaRecorder:
+        return MediaRecorder(output_video_file)
+
+
+ctx = webrtc_streamer(
+                        key="Squats-pose-analysis",
+                        video_frame_callback=video_frame_callback,
+                        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},  # Add this config
+                        media_stream_constraints={"video": {"width": {'min':480, 'ideal':480}}, "audio": False},
+                        video_html_attrs=VideoHTMLAttributes(autoPlay=True, controls=False, muted=False),
+                        out_recorder_factory=out_recorder_factory
+                    )
+
+
+download_button = st.empty()
+
+if os.path.exists(output_video_file):
+    with open(output_video_file, 'rb') as op_vid:
+        download = download_button.download_button('Download Video', data = op_vid, file_name='output_live.flv')
+
+        if download:
+            st.session_state['download'] = True
+
+
+
+if os.path.exists(output_video_file) and st.session_state['download']:
+    os.remove(output_video_file)
+    st.session_state['download'] = False
+    download_button.empty()
+
     
     
 
